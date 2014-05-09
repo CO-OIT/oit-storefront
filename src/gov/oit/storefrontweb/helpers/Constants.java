78fed659af8d9d1aa7fb8d96c04e70b033d1f3a7
woof <Wolf.Thompson@state.co.us>
2014-05-09 09:24:07 -0600

    Constants library

---
+++ src/gov/oit/storefrontweb/helpers/Constants.java
@@ -0,0 +1,9 @@
+package gov.oit.storefrontweb.helpers;
+
+public class Constants {
+
+	public static final String ENT_APP_INFO_DETAIL = "AppInfoDetail";
+	public static final String ENT_APP_INFO_CATEGORY = "AppInfoCategory";
+	
+	
+}
